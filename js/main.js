b2face733d3892bb93fb12076e1bb7994cf8a041
omar <omar@OJ-Macbook-Pro.local>
2026-06-17 15:50:56 -0400

    Polish homepage scrollybook and nav

--- js/main.js
+++ js/main.js
@@ -508,6 +508,7 @@ function renderChrome() {
         ${links.map(navItem).join("")}
       </nav>
       <div class="nav-actions">
+        <a class="nav-account" href="${root}account.html"><span>Sign in</span></a>
         <a class="nav-cart" href="${root}cart.html" aria-label="Open cart"><span>Cart</span><b class="cart-count" data-cart-count hidden>0</b></a>
         <button class="nav-burger" id="navBurger" aria-label="Menu" aria-expanded="false" aria-controls="navLinks"><span></span><span></span><span></span></button>
       </div>
