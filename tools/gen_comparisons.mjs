1b131e616fa1c82c71f42e2a23d384300fdc569d
OJamals <omarjam9393@gmail.com>
2026-07-10 23:24:35 -0400

    fix: publish fresh customer chat entrypoint

--- tools/gen_comparisons.mjs
+++ tools/gen_comparisons.mjs
@@ -262,7 +262,7 @@ function pageHtml(page) {
   </section>
 </main>
 
-<script type="module" src="../js/main.js?v=20260711b"></script>
+<script type="module" src="../js/main.js?v=20260711c"></script>
 <script src="../js/track.js" defer></script>
 </body>
 </html>
--- tools/seo-inject.mjs
+++ tools/seo-inject.mjs
@@ -572,7 +572,7 @@ ${jsonLd(productSchema(id, product, reviewsSnapshot))}
     </div>
   </section>
 </main>
-<script type="module" src="../js/main.js?v=20260711b"></script>
+<script type="module" src="../js/main.js?v=20260711c"></script>
 <script src="../js/track.js" defer></script>
 </body>
 </html>
